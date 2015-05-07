2f0cc02d60b8f7b5188334ed9f8aacbc0f5cae51
Ari Suutari <ari@stonepile.fi>
2015-05-07 17:11:20 +0300

    Move settings that are related to newlib into app directory.

--- msp432-launchpad/mpconfigapp.h
+++ msp432-launchpad/mpconfigapp.h
@@ -1,3 +1,10 @@
+#define MICROPY_PY_IO               (1)
+#define MICROPY_PY_IO_FILEIO        (1)
+#define MICROPY_HELPER_LEXER_UNIX   (1)
+
+#define MICROPY_HW_BOARD_NAME "Ti Msp432 Launchpad"
+#define MICROPY_HW_MCU_NAME "Arm Cortex-M4"
+
 extern const struct _mp_obj_module_t mp_module_launchpad;
 
 #define MICROPY_APP_BUILTIN_MODULES \
