18a38ca333872f11956cb66e2fb9f35754190b58
Ari Suutari <ari@stonepile.fi>
2017-11-15 18:05:58 +0200

    Add test version for pico]OS unix version.

---
+++ unix/mpconfigapp.h
@@ -0,0 +1,13 @@
+#define MICROPY_PY_IO               (1)
+#define MICROPY_PY_IO_FILEIO        (1)
+#define MICROPY_HELPER_LEXER_UNIX   (1)
+#define MICROPY_READER_POSIX        (1)
+#define MICROPY_PY_SYS_STDFILES     (1)
+
+
+#define MICROPY_HW_BOARD_NAME "Unix Box"
+#define MICROPY_HW_MCU_NAME "x64"
+
+#define MICROPY_APP_BUILTIN_MODULES 
+
+
---
+++ unix/mptask.c
@@ -0,0 +1,102 @@
+/*
+ * This file is part of the Micro Python project, http://micropython.org/
+ *
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2013, 2014 Damien P. George
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+
+/*
+ * This stuff has been gathered from other MicroPython ports. It 
+ * needs some clennup. It allows running python on Pico]OS console.
+ */
+
+#include <picoos.h>
+
+#include <stdint.h>
+#include <stdio.h>
+#include <string.h>
+#include <termios.h>
+#include <fcntl.h>
+
+#include <sys/stat.h>
+
+#include "py/nlr.h"
+#include "py/compile.h"
+#include "py/runtime.h"
+#include "py/repl.h"
+#include "py/gc.h"
+#include "genhdr/mpversion.h"
+#include "ports/unix/input.h"
+
+static char *stack_top;
+    #if MICROPY_ENABLE_GC
+static char heap[10240];
+#endif
+
+int mp_main(int argc, char **argv) {
+    int stack_dummy;
+    stack_top = (char*)&stack_dummy;
+
+    #if MICROPY_ENABLE_GC
+    gc_init(heap, heap + sizeof(heap));
+    #endif
+    mp_init();
+
+    struct termios oldtio,newtio;
+
+    tcgetattr(0,&oldtio);
+    tcgetattr(0,&newtio);
+
+    cfmakeraw(&newtio);
+    newtio.c_cc[VMIN]=1;
+    newtio.c_cc[VTIME]=0;
+
+    tcsetattr(0,TCSANOW,&newtio);
+
+    pyexec_friendly_repl();
+
+    tcsetattr(0,TCSANOW,&oldtio);
+
+    mp_deinit();
+    exit(0);
+    return 0;
+}
+
+
+mp_import_stat_t mp_import_stat(const char *path) {
+    struct stat st;
+    if (stat(path, &st) == 0) {
+        if (S_ISDIR(st.st_mode)) {
+            return MP_IMPORT_STAT_DIR;
+        } else if (S_ISREG(st.st_mode)) {
+            return MP_IMPORT_STAT_FILE;
+        }
+    }
+    return MP_IMPORT_STAT_NO_EXIST;
+
+}
+
+void nlr_jump_fail(void *val) {
+    printf("FATAL: uncaught NLR %p\n", val);
+    exit(1);
+}
